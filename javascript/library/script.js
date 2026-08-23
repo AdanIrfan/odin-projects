670366b59cfcd9a920f760e0c12e9545244ad062
AdanIrfan <176397003+AdanIrfan@users.noreply.github.com>
2026-08-24 00:50:17 +0500

    Complete javascript library project

--- javascript/library/script.js
+++ javascript/library/script.js
@@ -11,6 +11,17 @@ function Book(name, author, pages, read){
     this.read = read
 }
 
+Book.prototype.toggleRead = function(){
+    if(this.read === true){
+        this.read = false;
+        displayBook();
+    }
+    else{
+        this.read = true;
+        displayBook();
+    }
+}
+
 function addBookToLibrary(name, author, pages, read){
     let book = new Book(name, author, pages, read)
     mylibrary.push(book);
@@ -18,6 +29,7 @@ function addBookToLibrary(name, author, pages, read){
 
 function displayBook(){
     const container = document.querySelector(".container")
+    container.textContent = "";
     for( const book of mylibrary ){
         const card = document.createElement("div");
         card.classList.add("card");
@@ -31,13 +43,53 @@ function displayBook(){
         const pages = document.createElement("p");
         pages.textContent = `${book.pages}`
         card.appendChild(pages);
-        const read = document.createElement("p");
+        const toggleBtn = document.createElement("button");
         if(book.read === true){
-            read.textContent = `Book is already read`
-            card.appendChild(read);
+            toggleBtn.textContent = `Read`;
         }
+        else{
+            toggleBtn.textContent = `UnRead`;
+        }
+        toggleBtn.addEventListener("click", () =>{ 
+            book.toggleRead();
+        });
+        card.appendChild(toggleBtn);
+        const id = book.id;
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        removeBtn.addEventListener("click", () =>{
+            const index = mylibrary.findIndex((book) => book.id == id);
+            mylibrary.splice(index,1);
+            displayBook();   
+        })
+        card.appendChild(removeBtn);
     }
 }
+
+const openbtn = document.getElementById("openbtn");
+const dialog = document.getElementById("dialog");
+const closebtn = document.getElementById("closebtn");
+const submitbtn = document.getElementById("submitbtn");
+
+openbtn.addEventListener("click", ()=> dialog.showModal());
+closebtn.addEventListener("click", ()=> dialog.close());
+submitbtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    let read = document.querySelector("input[name = read]:checked").value;
+    if(read === 'yes'){
+        read = true;
+    }
+    else{
+        read = false;
+    }
+    addBookToLibrary(title, author, Number(pages), read);
+    dialog.close();
+    displayBook();
+})
+
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
 addBookToLibrary("1984", "George Orwell", 328, true);
